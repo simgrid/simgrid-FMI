a7b314163b8123f18ed41fa59208b5b07c599cce
bcamus <benjamin.camus@inria.fr>
2018-06-28 11:57:21 +0200

    MAJ de l'exemple thermal_system selon la nouvelle syntaxe SimGrid + Ajout de l'exemple thermal_system dans le cmake

--- examples/thermal-cloud/s4u-thermal-cloud.cpp
+++ examples/thermal-cloud/s4u-thermal-cloud.cpp
@@ -1,6 +1,6 @@
 #include "simgrid/plugins/energy.h"
 #include "simgrid/s4u.hpp"
-#include "simgrid/plugins/fmi.hpp"
+#include "simgrid-fmi.hpp"
 #include "simgrid/s4u/VirtualMachine.hpp"
 #include "simgrid/plugins/live_migration.h"
 #include <string>
@@ -37,7 +37,7 @@ public:
 
 	static void logOutput(){
 
-		output << simgrid::s4u::Engine::getClock() << ";";
+		output << simgrid::s4u::Engine::get_clock() << ";";
 
 		output << getDCPowerConsumption("rennes") << ";";
 		output << getDCPowerConsumption("sophia") << ";";
@@ -92,7 +92,7 @@ static void manageFailure(std::vector<std::string> args){
 
 	// then, we wake up the actor
 	unsigned long pid_on = std::stoul(args[2],0,10);
-	simgrid::s4u::Actor::byPid(pid_on)->resume();
+	simgrid::s4u::Actor::by_pid(pid_on)->resume();
 }
 
 
@@ -101,7 +101,7 @@ static void wakeUpActor(std::vector<std::string> args){
 	Utility::logOutput();
 	// then, we wake up the actor
 	unsigned long pid_on = std::stoul(args[2],0,10);
-	simgrid::s4u::Actor::byPid(pid_on)->resume();
+	simgrid::s4u::Actor::by_pid(pid_on)->resume();
 }
 
 
@@ -111,7 +111,7 @@ static void wakeUpActor(std::vector<std::string> args){
 static void shutDownRennesHosts(std::vector<std::string> args){
 
 
-	unsigned long pid = simgrid::s4u::Actor::self()->getPid();
+	unsigned long pid = simgrid::s4u::Actor::self()->get_pid();
 	std::vector<std::string> args_shutdown = {"thermal_system","power_supply_status",std::to_string(pid)};
 	simgrid::fmi::FMIPlugin::getFMU("thermal_system")->registerEvent(reactOnZeroValue,wakeUpActor,args_shutdown);
 	simgrid::s4u::Actor::self()->suspend();
@@ -126,12 +126,12 @@ static void shutDownRennesHosts(std::vector<std::string> args){
 	for(int i=0;i<nb_hosts_per_cluster;i++){
 		std::string host_name = "c-" + std::to_string(i) + ".rennes";
 		simgrid::s4u::Host* host = simgrid::s4u::Host::by_name(host_name);
-		if(host->isOn())
-			host->turnOff();
+		if(host->is_on())
+			host->turn_off();
 	}
 
 	unsigned long pid_on = std::stoul(args[0],0,10);
-	simgrid::s4u::Actor::byPid(pid_on)->kill();
+	simgrid::s4u::Actor::by_pid(pid_on)->kill();
 
 	Utility::updatePowerInFMUs();
 
@@ -164,12 +164,12 @@ static int master_nominal_behavior(std::vector<std::string> args){
 			simgrid::s4u::Host* host = simgrid::s4u::Host::by_name(host_name);
 
 			simgrid::s4u::VirtualMachine* vm = new simgrid::s4u::VirtualMachine(vm_name.c_str(), host, vm_core_nb);
-			vm->setRamsize(vm_ram_size);
+			vm->set_ramsize(vm_ram_size);
 			vm->start();
 
 			for(int j=0;j<vm_core_nb;j++){
 				std::string actor_name = std::to_string(j) + "_" + vm_name;
-				simgrid::s4u::Actor::createActor(vm_name.c_str(), vm , slave_behavior, args);
+				simgrid::s4u::Actor::create(vm_name.c_str(), vm , slave_behavior, args);
 			}
 
 			running_vms.push_back(vm);
@@ -189,7 +189,7 @@ static int master_nominal_behavior(std::vector<std::string> args){
 
 static int failureNotifier(std::vector<std::string> args){
 
-	unsigned long pid = simgrid::s4u::Actor::self()->getPid();
+	unsigned long pid = simgrid::s4u::Actor::self()->get_pid();
 
 	std::vector<std::string> params = {"chiller_failure","chiller_status",std::to_string(pid)};
 
@@ -198,9 +198,9 @@ static int failureNotifier(std::vector<std::string> args){
 
 	XBT_INFO("failure of the chiller detected!!! send a message to notify the failure manager ");
 	double* payload = new double();
-	*payload        = simgrid::s4u::Engine::getClock();
+	*payload        = simgrid::s4u::Engine::get_clock();
 
-	simgrid::s4u::Mailbox::byName("emergency")->put(payload,200);
+	simgrid::s4u::Mailbox::by_name("emergency")->put(payload,200);
 
 	return 0;
 }
@@ -208,19 +208,19 @@ static int failureNotifier(std::vector<std::string> args){
 
 static int failureManager(std::vector<std::string> args){
 
-	simgrid::s4u::Mailbox::byName("emergency")->get();
+	simgrid::s4u::Mailbox::by_name("emergency")->get();
 
 	unsigned long pid_current_manager = std::stoul(args[0],0,10);
 
 	// stop deploying VMs you @%^&"A§* !!!!
-	simgrid::s4u::Actor::byPid(pid_current_manager)->kill();
+	simgrid::s4u::Actor::by_pid(pid_current_manager)->kill();
 
 	// start shutting down unused PM
 	for(int i=current_host+1;i<nb_hosts_per_cluster;i++){
 		std::string host_name = "c-" + std::to_string(i) + ".rennes";
 		simgrid::s4u::Host* host = simgrid::s4u::Host::by_name(host_name);
-		if(host->isOn())
-			host->turnOff();
+		if(host->is_on())
+			host->turn_off();
 	}
 	Utility::updatePowerInFMUs();
 
@@ -231,9 +231,9 @@ static int failureManager(std::vector<std::string> args){
 		std::string dest_name = "c-" + std::to_string(current_host) + ".sophia";
 		simgrid::s4u::Host* dest = simgrid::s4u::Host::by_name(dest_name);
 
-		XBT_INFO("start migration of vm %s (status %i) to host %s",vm->getName().c_str(),vm->getState(),dest->getName().c_str());
+		XBT_INFO("start migration of vm %s to host %s",vm->get_name().c_str(),dest->get_name().c_str());
 		sg_vm_migrate(vm, dest);
-		XBT_INFO("end of migration of vm %s to host %s",vm->getName().c_str(),dest_name.c_str());
+		XBT_INFO("end of migration of vm %s to host %s",vm->get_name().c_str(),dest_name.c_str());
 
 		vm_id++;
 
@@ -241,7 +241,7 @@ static int failureManager(std::vector<std::string> args){
 			std::string host_name = "c-" + std::to_string(current_host) + ".rennes";
 			simgrid::s4u::Host* host = simgrid::s4u::Host::by_name(host_name);
 			XBT_INFO("turning OFF host %s which is empty",host_name.c_str());
-			host->turnOff();
+			host->turn_off();
 			current_host++;
 			vm_id=0;
 		}
@@ -273,7 +273,7 @@ int main(int argc, char *argv[])
 
   simgrid::s4u::Engine e(&argc, argv);
 
-  e.loadPlatform("clusters_rennes.xml");
+  e.load_platform("clusters_rennes.xml");
 
   std::vector<std::string> args;
 
@@ -300,7 +300,8 @@ int main(int argc, char *argv[])
 
   // ADDING FMU-CS
 
-  std::string fmu_uri_2 = "file:///home/mecsyco/Documents/thermal_system";
+  //std::string fmu_uri_2 = "file:///home/mecsyco/git/simgrid-FMI/examples/thermal-cloud/thermal_system";
+  std::string fmu_uri_2 = "file:///home/mecsyco/Documents/fmu_test/thermal_system_om";
   std::string fmu_name_2 = "thermal_system";
 
   const double stepsize_2 = 0.01;
@@ -315,6 +316,8 @@ int main(int argc, char *argv[])
   std::vector<std::string> realOutputNames_2 = {"Q_load_DC","P_other_DC","Q_other_DC","P_chiller","Q_cooling","P_DC","Q_DC","T_R_out"};
   std::vector<std::string> intOutputNames_2 = { "power_supply_status" };
 
+  XBT_INFO("trying to instantiate FMU 2");
+
   simgrid::fmi::FMIPlugin::addFMUCS(fmu_uri_2, fmu_name_2, intstepsize_2,
    		  &initRealInputNames_2, &initRealInputVals_2,
 		  &initIntInputNames_2, &initIntInputVals_2,
@@ -322,15 +325,19 @@ int main(int argc, char *argv[])
    		  nullptr, nullptr,
 		  &realOutputNames_2, &intOutputNames_2, nullptr, nullptr);
 
+  XBT_INFO("FMU 2 instantiated");
+
   Utility::updatePowerInFMUs();
 
-  simgrid::s4u::ActorPtr master_nominal = simgrid::s4u::Actor::createActor("master_nominal", simgrid::s4u::Host::by_name("c-0.sophia"), master_nominal_behavior, args);
-  simgrid::s4u::Actor::createActor("failure_notifier", simgrid::s4u::Host::by_name("c-0.rennes"), failureNotifier, args);
-  std::vector<std::string> args_failure_manager = {std::to_string(master_nominal.get()->getPid())};
-  simgrid::s4u::ActorPtr failure_manager = simgrid::s4u::Actor::createActor("failure_manager", simgrid::s4u::Host::by_name("c-0.sophia"), failureManager, args_failure_manager);
 
-  std::vector<std::string> args_shutdown = {std::to_string(failure_manager.get()->getPid())};
-  simgrid::s4u::Actor::createActor("shutdown_process", simgrid::s4u::Host::by_name("c-0.sophia"), shutDownRennesHosts, args_shutdown);
+
+  simgrid::s4u::ActorPtr master_nominal = simgrid::s4u::Actor::create("master_nominal", simgrid::s4u::Host::by_name("c-0.sophia"), master_nominal_behavior, args);
+  simgrid::s4u::Actor::create("failure_notifier", simgrid::s4u::Host::by_name("c-0.rennes"), failureNotifier, args);
+  std::vector<std::string> args_failure_manager = {std::to_string(master_nominal.get()->get_pid())};
+  simgrid::s4u::ActorPtr failure_manager = simgrid::s4u::Actor::create("failure_manager", simgrid::s4u::Host::by_name("c-0.sophia"), failureManager, args_failure_manager);
+
+  std::vector<std::string> args_shutdown = {std::to_string(failure_manager.get()->get_pid())};
+  simgrid::s4u::Actor::create("shutdown_process", simgrid::s4u::Host::by_name("c-0.sophia"), shutDownRennesHosts, args_shutdown);
 
   e.run();
 
